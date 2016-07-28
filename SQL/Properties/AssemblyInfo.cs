0db0b3e8172d8f490d53aee7a2858a809d4658fb
YetaWF <mikevdm@YetaWF.com>
2016-07-28 09:30:37 -0400

    preparing for 1.0.2 release (easier to keep all versions for assemblies in sync, not a requirement, but easier to manage)

--- SQL/Properties/AssemblyInfo.cs
+++ SQL/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using YetaWF.PackageAttributes;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
 
 [assembly: Package(PackageTypeEnum.DataProvider, "YetaWF")]
 
