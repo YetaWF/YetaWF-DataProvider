68cd3f29f1df72d45d99ab4ca250dbc1caefbc46
YetaWF <mikevdm@YetaWF.com>
2016-08-12 08:16:25 -0400

    prepare for 1.0.3 release

--- EF/Properties/AssemblyInfo.cs
+++ EF/Properties/AssemblyInfo.cs
@@ -12,6 +12,6 @@ using YetaWF.PackageAttributes;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
 
 [assembly: Package(PackageTypeEnum.DataProvider, "YetaWF")]
