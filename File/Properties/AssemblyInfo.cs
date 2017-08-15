cee98d125b335678d542858c80f675394df55d87
YetaWF <mikevdm@YetaWF.com>
2017-08-15 09:00:03 -0400

    prepare for 2.6.1

--- File/Properties/AssemblyInfo.cs
+++ File/Properties/AssemblyInfo.cs
@@ -12,6 +12,6 @@ using YetaWF.PackageAttributes;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.6.0.0")]
+[assembly: AssemblyVersion("2.6.1.0")]
 
 [assembly: Package(PackageTypeEnum.DataProvider, "YetaWF")]
