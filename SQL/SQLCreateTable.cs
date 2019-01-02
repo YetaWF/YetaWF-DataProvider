d042d322ae88ba104e3d64cb2df0217d419e6fbf
YetaWF <mikevdm@YetaWF.com>
2019-01-02 17:50:48 -0500

    2019

--- SQL/SQLCreateTable.cs
+++ SQL/SQLCreateTable.cs
@@ -1,4 +1,4 @@
-﻿/* Copyright © 2018 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Licensing */
+﻿/* Copyright © 2019 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Licensing */
 
 using Microsoft.SqlServer.Management.Smo;
 using System;
