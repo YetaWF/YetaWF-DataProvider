432e2fc4b3fe8faca6545e4b3ad13be54bbe96e1
YetaWF <mikevdm@YetaWF.com>
2019-03-25 15:42:16 -0400

    PostgreSQL moduledefinition

---
+++ ModuleDefinition/PostgreSQLDataProvider.cs
@@ -0,0 +1,35 @@
+﻿/* Copyright © 2019 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Licensing */
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using YetaWF.Core.DataProvider;
+using YetaWF.Core.Modules;
+using YetaWF.Core.Serializers;
+
+namespace YetaWF.DataProvider.PostgreSQL {
+
+    internal class PostgreSQLDataProvider {
+
+        internal class ModuleDefinitionDataProvider<KEY, TYPE> : PostgreSQLModuleObject<KEY, TYPE>, ModuleDefinitionDataProviderIOMode {
+
+            public ModuleDefinitionDataProvider(Dictionary<string, object> options) : base(options) { }
+
+            public async Task<SerializableList<DesignedModule>> GetDesignedModulesAsync() {
+                using (PostgreSQLSimpleObject<Guid, TempDesignedModule> dp = new PostgreSQLSimpleObject<Guid, TempDesignedModule>(Options)) {
+                    DataProviderGetRecords<TempDesignedModule> modules = await dp.GetRecordsAsync(0, 0, null, null);
+                    SerializableList<DesignedModule> list = new SerializableList<DesignedModule>();
+                    foreach (TempDesignedModule mod in modules.Data) {
+                        list.Add(new DesignedModule {
+                            ModuleGuid = mod.ModuleGuid,
+                            Name = mod.Name,
+                            Description = mod.Description,
+                            AreaName = mod.DerivedAssemblyName.Replace(".", "_"),
+                        });
+                    }
+                    return list;
+                }
+            }
+        }
+    }
+}
