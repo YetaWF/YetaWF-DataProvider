9e6393bfbdc747624418c7dfc55039d69f30303b
Mike <mikevdm@mikevdm.com>
2020-06-27 08:59:20 -0400

    sqldyn initialization fix

--- SQLDyn/Properties/AssemblyInfo.cs
+++ SQLDyn/Properties/AssemblyInfo.cs
@@ -3,12 +3,12 @@
 using System.Reflection;
 using YetaWF.PackageAttributes;
 
-[assembly: AssemblyTitle("SQL")]
+[assembly: AssemblyTitle("SQLDyn")]
 [assembly: AssemblyDescription("YetaWF SQL Data Provider")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Softel vdm, Inc.")]
 [assembly: AssemblyCopyright("Copyright © 2020 - Softel vdm, Inc.")]
-[assembly: AssemblyProduct("SQL")]
+[assembly: AssemblyProduct("SQLDyn")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
